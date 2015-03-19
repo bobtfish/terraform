69b4bf979f8243b38c9bfe15467621d5ae888db4
Radek Simko <radek.simko@gmail.com>
2015-03-19 22:38:22 +0000

    Compatibility layer with goamz added

--- builtin/providers/aws/config.go
+++ builtin/providers/aws/config.go
@@ -3,6 +3,7 @@ package aws
 import (
 	"fmt"
 	"log"
+	"os"
 	"time"
 
 	"github.com/hashicorp/aws-sdk-go/aws"
@@ -36,6 +37,7 @@ type AWSClient struct {
 }
 
 func (c *Config) loadAndValidate(providerCode string) (interface{}, error) {
+	c.tryLoadingDeprecatedEnvVars()
 	credsProvider, err := c.getCredsProvider(providerCode)
 	if err != nil {
 		return nil, err
@@ -48,6 +50,19 @@ func (c *Config) loadAndValidate(providerCode string) (interface{}, error) {
 	c.Provider = credsProvider
 
 	return c.Client()
+}
+
+func (c *Config) tryLoadingDeprecatedEnvVars() {
+	// Backward compatibility
+	if c.Token == "" {
+		c.Token = os.Getenv("AWS_SECURITY_TOKEN")
+	}
+	if c.CredentialsFilePath == "" {
+		c.CredentialsFilePath = os.Getenv("AWS_CREDENTIAL_FILE")
+	}
+	if c.CredentialsFileProfile == "" {
+		c.CredentialsFileProfile = os.Getenv("AWS_PROFILE")
+	}
 }
 
 func (c *Config) getCredsProvider(providerCode string) (aws.CredentialsProvider, error) {
