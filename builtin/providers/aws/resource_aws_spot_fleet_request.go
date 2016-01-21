513889b5f44860e00bf1850ac5e5a9400eb0e682
Kyle Anderson <kwa@yelp.com>
2016-01-21 14:35:59 -0800

    Use the right objects and methods, make tests pass

--- builtin/providers/aws/resource_aws_spot_fleet_request.go
+++ builtin/providers/aws/resource_aws_spot_fleet_request.go
@@ -8,7 +8,6 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/service/ec2"
-	"github.com/hashicorp/terraform/helper/resource"
 	"github.com/hashicorp/terraform/helper/schema"
 )
 
@@ -19,150 +18,88 @@ func resourceAwsSpotFleetRequest() *schema.Resource {
 		Delete: resourceAwsSpotFleetRequestDelete,
 		Update: resourceAwsSpotFleetRequestUpdate,
 
-		Schema: func() map[string]*schema.Schema {
-			// The Spot Fleet Request Schema is based on the AWS Instance schema.
-			s := resourceAwsInstance().Schema
-
-			// Everything on a spot instance is ForceNew except tags
-			for k, v := range s {
-				if k == "tags" {
-					continue
-				}
-				v.ForceNew = true
-			}
-
-			s["spot_price"] = &schema.Schema{
+		Schema: map[string]*schema.Schema{
+			"iam_fleet_role": &schema.Schema{
 				Type:     schema.TypeString,
 				Required: true,
 				ForceNew: true,
-			}
-			s["spot_type"] = &schema.Schema{
-				Type:     schema.TypeString,
-				Optional: true,
-				Default:  "persistent",
-			}
-			s["wait_for_fulfillment"] = &schema.Schema{
-				Type:     schema.TypeBool,
-				Optional: true,
-				Default:  false,
-			}
-			s["spot_bid_status"] = &schema.Schema{
-				Type:     schema.TypeString,
-				Computed: true,
-			}
-			s["spot_request_state"] = &schema.Schema{
-				Type:     schema.TypeString,
-				Computed: true,
-			}
-			s["spot_instance_id"] = &schema.Schema{
-				Type:     schema.TypeString,
-				Computed: true,
-			}
-			s["block_duration_minutes"] = &schema.Schema{
-				Type:     schema.TypeInt,
-				Optional: true,
+			},
+			"launch_specifications": &schema.Schema{
+				Type:     schema.TypeList,
+				Required: true,
 				ForceNew: true,
-			}
-
-			return s
-		}(),
+				// TODO: Figure out what the right schema is for this list
+				Elem: &schema.Schema{Type: schema.TypeString},
+			},
+			"spot_price": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			// Everything on a spot fleet is ForceNew except target_capacity
+			"target_capacity": &schema.Schema{
+				Type:     schema.TypeInt,
+				Required: true,
+				ForceNew: false,
+			},
+			// TODO: AllocationStrategy
+			// TODO: ClientToken?
+			// TODO: ExcessCapacityTerminationPolicy
+			// TODO: TerminateInstancesWithExpiration
+			// TODO: ValidFrom
+			// TODO: ValidUntil
+		},
 	}
 }
 
 func resourceAwsSpotFleetRequestCreate(d *schema.ResourceData, meta interface{}) error {
+	// http://docs.aws.amazon.com/AWSEC2/latest/APIReference/API_RequestSpotFleet.html
 	conn := meta.(*AWSClient).ec2conn
 
-	instanceOpts, err := buildAwsInstanceOpts(d, meta)
+	// http://docs.aws.amazon.com/sdk-for-go/api/service/ec2.html#type-SpotFleetRequestConfigData
+	spotFleetConfig := &ec2.SpotFleetRequestConfigData{
+		IamFleetRole:                     aws.String(d.Get("iam_fleet_role").(string)),
+		LaunchSpecifications:             []*ec2.SpotFleetLaunchSpecification{},
+		SpotPrice:                        aws.String(d.Get("spot_price").(string)),
+		TargetCapacity:                   aws.Int64(1), // Required
+		AllocationStrategy:               aws.String("AllocationStrategy"),
+		ClientToken:                      aws.String("String"),
+		ExcessCapacityTerminationPolicy:  aws.String("ExcessCapacityTerminationPolicy"),
+		TerminateInstancesWithExpiration: aws.Bool(true),
+		ValidFrom:                        aws.Time(time.Now()),
+		ValidUntil:                       aws.Time(time.Now()),
+	}
+
+	// http://docs.aws.amazon.com/sdk-for-go/api/service/ec2.html#type-RequestSpotFleetInput
+	spotFleetOpts := &ec2.RequestSpotFleetInput{
+		SpotFleetRequestConfig: spotFleetConfig,
+		DryRun:                 aws.Bool(false),
+	}
+
+	log.Printf("[DEBUG] Requesting spot fleet with these opts: %s", spotFleetOpts)
+	resp, err := conn.RequestSpotFleet(spotFleetOpts)
 	if err != nil {
-		return err
+		return fmt.Errorf("Error requesting spot fleet: %s", err)
 	}
 
-	spotOpts := &ec2.RequestSpotInstancesInput{
-		SpotPrice: aws.String(d.Get("spot_price").(string)),
-		Type:      aws.String(d.Get("spot_type").(string)),
-
-		// Though the AWS API supports creating spot instance requests for multiple
-		// instances, for TF purposes we fix this to one instance per request.
-		// Users can get equivalent behavior out of TF's "count" meta-parameter.
-		InstanceCount: aws.Int64(1),
-
-		LaunchSpecification: &ec2.RequestSpotLaunchSpecification{
-			BlockDeviceMappings: instanceOpts.BlockDeviceMappings,
-			EbsOptimized:        instanceOpts.EBSOptimized,
-			Monitoring:          instanceOpts.Monitoring,
-			IamInstanceProfile:  instanceOpts.IAMInstanceProfile,
-			ImageId:             instanceOpts.ImageID,
-			InstanceType:        instanceOpts.InstanceType,
-			KeyName:             instanceOpts.KeyName,
-			Placement:           instanceOpts.SpotPlacement,
-			SecurityGroupIds:    instanceOpts.SecurityGroupIDs,
-			SecurityGroups:      instanceOpts.SecurityGroups,
-			SubnetId:            instanceOpts.SubnetID,
-			UserData:            instanceOpts.UserData64,
-		},
-	}
-
-	if v, ok := d.GetOk("block_duration_minutes"); ok {
-		spotOpts.BlockDurationMinutes = aws.Int64(int64(v.(int)))
-	}
-
-	// If the instance is configured with a Network Interface (a subnet, has
-	// public IP, etc), then the instanceOpts.SecurityGroupIds and SubnetId will
-	// be nil
-	if len(instanceOpts.NetworkInterfaces) > 0 {
-		spotOpts.LaunchSpecification.SecurityGroupIds = instanceOpts.NetworkInterfaces[0].Groups
-		spotOpts.LaunchSpecification.SubnetId = instanceOpts.NetworkInterfaces[0].SubnetId
-	}
-
-	// Make the spot instance request
-	log.Printf("[DEBUG] Requesting spot bid opts: %s", spotOpts)
-	resp, err := conn.RequestSpotInstances(spotOpts)
-	if err != nil {
-		return fmt.Errorf("Error requesting spot instances: %s", err)
-	}
-	if len(resp.SpotInstanceRequests) != 1 {
-		return fmt.Errorf(
-			"Expected response with length 1, got: %s", resp)
-	}
-
-	sir := *resp.SpotInstanceRequests[0]
-	d.SetId(*sir.SpotInstanceRequestId)
-
-	if d.Get("wait_for_fulfillment").(bool) {
-		spotStateConf := &resource.StateChangeConf{
-			// http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/spot-bid-status.html
-			Pending:    []string{"start", "pending-evaluation", "pending-fulfillment"},
-			Target:     "fulfilled",
-			Refresh:    SpotInstanceStateRefreshFunc(conn, sir),
-			Timeout:    10 * time.Minute,
-			Delay:      10 * time.Second,
-			MinTimeout: 3 * time.Second,
-		}
-
-		log.Printf("[DEBUG] waiting for spot bid to resolve... this may take several minutes.")
-		_, err = spotStateConf.WaitForState()
-
-		if err != nil {
-			return fmt.Errorf("Error while waiting for spot request (%s) to resolve: %s", sir, err)
-		}
-	}
+	d.SetId(*resp.SpotFleetRequestId)
 
 	return resourceAwsSpotFleetRequestUpdate(d, meta)
 }
 
-// Update spot state, etc
 func resourceAwsSpotFleetRequestRead(d *schema.ResourceData, meta interface{}) error {
+	// http://docs.aws.amazon.com/AWSEC2/latest/APIReference/API_DescribeSpotFleetRequests.html
 	conn := meta.(*AWSClient).ec2conn
 
-	req := &ec2.DescribeSpotInstanceRequestsInput{
-		SpotInstanceRequestIds: []*string{aws.String(d.Id())},
+	req := &ec2.DescribeSpotFleetRequestsInput{
+		SpotFleetRequestIds: []*string{aws.String(d.Id())},
 	}
-	resp, err := conn.DescribeSpotInstanceRequests(req)
+	resp, err := conn.DescribeSpotFleetRequests(req)
 
 	if err != nil {
 		// If the spot request was not found, return nil so that we can show
 		// that it is gone.
-		if ec2err, ok := err.(awserr.Error); ok && ec2err.Code() == "InvalidSpotInstanceRequestID.NotFound" {
+		if ec2err, ok := err.(awserr.Error); ok && ec2err.Code() == "InvalidSpotFleetRequestID.NotFound" {
 			d.SetId("")
 			return nil
 		}
@@ -171,89 +108,24 @@ func resourceAwsSpotFleetRequestRead(d *schema.ResourceData, meta interface{}) e
 		return err
 	}
 
-	// If nothing was found, then return no state
-	if len(resp.SpotInstanceRequests) == 0 {
-		d.SetId("")
-		return nil
-	}
-
-	request := resp.SpotInstanceRequests[0]
+	request := resp.SpotFleetRequestConfigs[0]
 
 	// if the request is cancelled, then it is gone
-	if *request.State == "cancelled" {
+	if *request.SpotFleetRequestState == "cancelled" {
 		d.SetId("")
 		return nil
 	}
 
-	d.Set("spot_bid_status", *request.Status.Code)
-	// Instance ID is not set if the request is still pending
-	if request.InstanceId != nil {
-		d.Set("spot_instance_id", *request.InstanceId)
-		// Read the instance data, setting up connection information
-		if err := readInstance(d, meta); err != nil {
-			return fmt.Errorf("[ERR] Error reading Spot Instance Data: %s", err)
-		}
-	}
-
-	d.Set("spot_request_state", request.State)
-	d.Set("block_duration_minutes", request.BlockDurationMinutes)
-	d.Set("tags", tagsToMap(request.Tags))
-
-	return nil
-}
-
-func readInstance(d *schema.ResourceData, meta interface{}) error {
-	conn := meta.(*AWSClient).ec2conn
-
-	resp, err := conn.DescribeInstances(&ec2.DescribeInstancesInput{
-		InstanceIds: []*string{aws.String(d.Get("spot_instance_id").(string))},
-	})
-	if err != nil {
-		// If the instance was not found, return nil so that we can show
-		// that the instance is gone.
-		if ec2err, ok := err.(awserr.Error); ok && ec2err.Code() == "InvalidInstanceID.NotFound" {
-			return fmt.Errorf("no instance found")
-		}
-
-		// Some other error, report it
-		return err
-	}
-
-	// If nothing was found, then return no state
-	if len(resp.Reservations) == 0 {
-		return fmt.Errorf("no instances found")
-	}
-
-	instance := resp.Reservations[0].Instances[0]
-
-	// Set these fields for connection information
-	if instance != nil {
-		d.Set("public_dns", instance.PublicDnsName)
-		d.Set("public_ip", instance.PublicIpAddress)
-		d.Set("private_dns", instance.PrivateDnsName)
-		d.Set("private_ip", instance.PrivateIpAddress)
-
-		// set connection information
-		if instance.PublicIpAddress != nil {
-			d.SetConnInfo(map[string]string{
-				"type": "ssh",
-				"host": *instance.PublicIpAddress,
-			})
-		} else if instance.PrivateIpAddress != nil {
-			d.SetConnInfo(map[string]string{
-				"type": "ssh",
-				"host": *instance.PrivateIpAddress,
-			})
-		}
-	}
-
+	d.Set("spot_request_state", request.SpotFleetRequestState)
 	return nil
 }
 
 func resourceAwsSpotFleetRequestUpdate(d *schema.ResourceData, meta interface{}) error {
+	// http://docs.aws.amazon.com/AWSEC2/latest/APIReference/API_ModifySpotFleetRequest.html
 	conn := meta.(*AWSClient).ec2conn
 
 	d.Partial(true)
+	// TODO: Adjust target capacity
 	if err := setTags(conn, d); err != nil {
 		return err
 	} else {
@@ -266,54 +138,18 @@ func resourceAwsSpotFleetRequestUpdate(d *schema.ResourceData, meta interface{})
 }
 
 func resourceAwsSpotFleetRequestDelete(d *schema.ResourceData, meta interface{}) error {
+	// http://docs.aws.amazon.com/AWSEC2/latest/APIReference/API_CancelSpotFleetRequests.html
 	conn := meta.(*AWSClient).ec2conn
 
-	log.Printf("[INFO] Cancelling spot request: %s", d.Id())
-	_, err := conn.CancelSpotInstanceRequests(&ec2.CancelSpotInstanceRequestsInput{
-		SpotInstanceRequestIds: []*string{aws.String(d.Id())},
+	log.Printf("[INFO] Cancelling spot fleet request: %s", d.Id())
+	_, err := conn.CancelSpotFleetRequests(&ec2.CancelSpotFleetRequestsInput{
+		SpotFleetRequestIds: []*string{aws.String(d.Id())},
 	})
 
 	if err != nil {
 		return fmt.Errorf("Error cancelling spot request (%s): %s", d.Id(), err)
 	}
 
-	if instanceId := d.Get("spot_instance_id").(string); instanceId != "" {
-		log.Printf("[INFO] Terminating instance: %s", instanceId)
-		if err := awsTerminateInstance(conn, instanceId); err != nil {
-			return fmt.Errorf("Error terminating spot instance: %s", err)
-		}
-	}
-
+	// TODO: Should we terminate or let the spot fleet policy do that?
 	return nil
-}
-
-// SpotInstanceStateRefreshFunc returns a resource.StateRefreshFunc that is used to watch
-// an EC2 spot instance request
-func SpotInstanceStateRefreshFunc(
-	conn *ec2.EC2, sir ec2.SpotInstanceRequest) resource.StateRefreshFunc {
-
-	return func() (interface{}, string, error) {
-		resp, err := conn.DescribeSpotInstanceRequests(&ec2.DescribeSpotInstanceRequestsInput{
-			SpotInstanceRequestIds: []*string{sir.SpotInstanceRequestId},
-		})
-
-		if err != nil {
-			if ec2err, ok := err.(awserr.Error); ok && ec2err.Code() == "InvalidSpotInstanceRequestID.NotFound" {
-				// Set this to nil as if we didn't find anything.
-				resp = nil
-			} else {
-				log.Printf("Error on StateRefresh: %s", err)
-				return nil, "", err
-			}
-		}
-
-		if resp == nil || len(resp.SpotInstanceRequests) == 0 {
-			// Sometimes AWS just has consistency issues and doesn't see
-			// our request yet. Return an empty state.
-			return nil, "", nil
-		}
-
-		req := resp.SpotInstanceRequests[0]
-		return req, *req.Status.Code, nil
-	}
 }
